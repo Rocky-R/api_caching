67db58f633f210e7422c620b410823bf2efaac2f
Rocky Randalls <rrandalls@gmail.com>
2015-03-04 21:35:21 -0500

    Add cache invalidation methods to Repositories#show

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,10 +6,12 @@ class RepositoriesController < ApplicationController
   def show
     @profile = Profile.find_by_username(params[:username]) ||
         Profile.create_from_username(params[:username])
+    @profile.destroy if @profile.updated_at < (Time.now - 24.hours)
+
     @repositories = Repository.where(profile_id: @profile.id).all
-    if @repositories.empty?
-      Repository.create_from_username(params[:username])
-    end
+    Repository.create_from_username(params[:username]) if @repositories.empty?
+    Repository.where(profile_id: @profile.id).delete_all if Repository.where(
+        profile_id: @profile.id).first.updated_at < (Time.now - 2.hours)
   end
 
 end
